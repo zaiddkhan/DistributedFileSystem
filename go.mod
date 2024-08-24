397d62437291cb176880a92e0b1c7b5be1a14f69
Zaid Khan <zaiddkhhan@gmail.com>
2024-08-24 10:05:05 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module DistributedFileSystems
+
+go 1.22
---
+++ p2p/transport.go
@@ -0,0 +1,11 @@
+package p2p
+
+// Peer is an interface that represents the remote node.
+type Peer interface {
+}
+
+// Transport is anything that handles the communication
+// between the nodes in the network. This can be o the form
+// (tcp ,udp ,web sockets)
+type Transport interface {
+}
